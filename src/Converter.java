e156bee15e00ffd2be233eaeec9b4279454ec865
this0is0kuba <jakub.rakus06@gmail.com>
2023-12-02 14:07:05 +0100

    add method to generate CSV for 2-var functions

--- src/Converter.java
+++ src/Converter.java
@@ -6,9 +6,14 @@ public class Converter {
 
     public static void main(String[] args) throws IOException {
 
-        String pathToData = "data/FUN3/FUN3-[0.0,99.0].dat";
-        String pathToOutputData = "outputData/FUN3/FUN3-[0.0,99.0].dat";
+//        String pathToData = "data/FUN3/FUN3-[0.0,99.0].dat";
+//        String pathToOutputData = "outputData/FUN3/FUN3-[0.0,99.0].dat";
+//
+//        DataFileConverter.convertToCSV(pathToData, pathToOutputData);
 
-        DataFileConverter.convertToCSV(pathToData, pathToOutputData);
+        String pathToData = "data/FUN6/FUN6-[-10.0,10.0]-[-10.0,10.0].dat";
+        String pathToOutputData = "outputData/FUN6/FUN6-[-10.0,10.0]-[-10.0,10.0].dat";
+
+        DataFileConverter.convertFunctionOf2VarToCSV(pathToData, pathToOutputData);
     }
 }
--- src/dataConverter/DataFileConverter.java
+++ src/dataConverter/DataFileConverter.java
@@ -1,16 +1,10 @@
 package dataConverter;
 
 import java.io.*;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 
 public class DataFileConverter {
-
-    public static final Map<String, String> converterOfVariables = Map.of(
-            "X1", "A",
-            "X2", "B",
-            "X3", "C"
-    );
 
     public static void convertToCSV(String pathToData, String pathToOutputData) throws IOException {
 
@@ -55,10 +49,8 @@ public class DataFileConverter {
             writer.write(lineFromData.replace(".", ",").replace(" ", "|") + "|");
 
             String tinyGPConvertedFunction = tinyGPFunction;
-            for(Map.Entry<String, String> entry : converterOfVariables.entrySet()) {
 
-                tinyGPConvertedFunction = tinyGPConvertedFunction.replace(entry.getKey(), entry.getValue() + i);
-            }
+            tinyGPConvertedFunction = tinyGPConvertedFunction.replace("X1", "A" + i);
 
             writer.write("=" + tinyGPConvertedFunction);
             writer.newLine();
@@ -67,5 +59,105 @@ public class DataFileConverter {
 
         writer.close();
         brOutputData.close();
+    }
+
+    public static void convertFunctionOf2VarToCSV(String pathToData, String pathToOutputData) throws IOException {
+
+        String[] names = pathToData.split("/");
+
+        String path = "convertedData/" + names[1] + "/";
+        String fileName = names[2].split(".dat")[0] + ".csv";
+        String fullPath = path + fileName;
+
+
+        //create folders
+        if(new File(path).mkdirs())
+            System.out.println("Folder directory: \"" +  path + "\" has been created");
+        else
+            System.out.println("Folder: \"" + path + "\" already exists");
+
+
+        //create file
+        File file = new File(fullPath);
+
+        if(file.createNewFile())
+            System.out.println("File: \"" +  file.getName() + "\" has been created");
+        else
+            System.out.println("File: \"" + file.getName() + "\" already exists");
+
+
+        BufferedReader brOutputData = new BufferedReader(new FileReader(pathToOutputData));
+        String tinyGPFunction = brOutputData.readLine();
+        brOutputData.close();
+
+        tinyGPFunction = tinyGPFunction.replace("X1", "B$1");
+        tinyGPFunction = tinyGPFunction.replace("X2",  "$A2");
+
+
+        BufferedReader brData = new BufferedReader(new FileReader(pathToData));
+        String headerFromData = brData.readLine();
+        String[] headerArray = headerFromData.split(" ");
+
+        List<String> inputValues = new ArrayList<>();
+        List<String> outputTargetValues = new ArrayList<>();
+
+        int i = 1;
+        String lineFromData;
+        while((lineFromData = brData.readLine()) != null) {
+
+            String[] parameters = lineFromData.split(" ");
+
+            inputValues.add(parameters[0]);
+            outputTargetValues.add(parameters[2]);
+
+            i++;
+        }
+
+        brData.close();
+
+        List<String> uniqueInputValues = inputValues.stream().distinct().toList();
+
+
+        BufferedWriter writer = new BufferedWriter(new FileWriter(fullPath));
+
+        // writing tinyGP Data for chart
+
+        for (String inputVal: uniqueInputValues)
+            writer.write( "|" + inputVal.replace(".", ","));
+
+        int j = 0;
+        for (String inputVal: uniqueInputValues) {
+
+            writer.newLine();
+            writer.write(inputVal.replace(".", ","));
+
+            if(j == 0)
+                writer.write( "|" + "=" + tinyGPFunction.replace(".", ","));
+
+            j++;
+        }
+
+        writer.newLine();
+        writer.newLine();
+
+        // writing target data for chart
+
+        for (String inputVal: uniqueInputValues)
+            writer.write( "|" + inputVal.replace(".", ","));
+
+        int k = 0;
+        for (String inputVal: uniqueInputValues) {
+
+            writer.newLine();
+            writer.write(inputVal.replace(".", ","));
+
+            for(int l = 0; l < uniqueInputValues.size(); l++) {
+
+                writer.write("|" + outputTargetValues.get(k * uniqueInputValues.size() + l).replace(".", ","));
+            }
+            k++;
+        }
+
+        writer.close();
     }
 }
